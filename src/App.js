67fd4a732084729ae20ec2db8b332b045bb81c69
unknown <qwop1606@gmail.com>
2020-07-07 22:43:39 +0300

    start project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './componets/Header/Header';
+import Box from './componets/Box/Profile';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Box />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componets/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className='container'><a href='#'><img src='https://about.canva.com/wp-content/uploads/sites/3/2016/08/logos-1.png'></img></a></div>
+        </header>
+    )
+}
+
+export default Header;
